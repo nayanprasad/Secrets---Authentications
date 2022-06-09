9cc942bd9e887a27513a85790afc6efcc2d438c9
Nayan Prasad P K <nayanprasad096@gmail.com>
2022-06-09 16:28:18 +0530

    level 1

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email : String,
+    password : String
+};
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) { 
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+
+    newUser.save(function (err) { 
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+     });
+
+ });
+
+app.post("/login", function (req, res) { 
+    User.findOne({email : req.body.username}, function (err, foundUser) { 
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === req.body.password){
+                    res.render("secrets");
+                }
+                else{
+                    console.log("incorrect password")
+                }
+            }
+            else{
+                console.log("no user found")
+            }
+           
+        }
+     });
+ }) ;
+
+
+app.listen("3000", function () {
+    console.log("server started...");
+})
